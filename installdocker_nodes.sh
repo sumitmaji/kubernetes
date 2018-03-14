fceef749ddf2c73857ec322bfefb830f6165dbdc
root <root@master.cloud.com>
2018-03-15 00:23:31 +0530

    Moved docker installation to install_docker

--- installdocker_nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ "TRACE" ]] && set -x
-
-: ${EXPORTDIR:=/home/sumit}
-
-mkdir -p /etc/docker/certs.d/master.cloud.com:5000
-
-cp "$EXPORTDIR"/certs/domain.crt /etc/docker/certs.d/master.cloud.com:5000/domain.crt
