f7fc5fed6a7bcc0b53eb65be67f7c83ee603fcb3
root <root@master.cloud.com>
2018-03-26 01:49:32 +0530

    Added hadoop yaml

--- deletePods.sh
+++ deletePods.sh
@@ -9,3 +9,11 @@ kubectl delete service kerberos
 kubectl delete deployment ldap-client-deployment
 kubectl delete service ldap-client
 
+kubectl delete deployment ldap-client1-deployment
+kubectl delete service ldap-client1
+
+kubectl delete deployment hadoop-deployment
+kubectl delete service hdfs-master
+
+kubectl delete deployment hadoop-slave01-deployment
+kubectl delete service slave01
