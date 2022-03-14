934fdbdf0d9076ad8a419affe84343f1d10c60bc
sumit <skmaji1@outlook.com>
2022-03-14 20:40:00 +0530

    updating kubernetes scripts for ubuntu 21

--- install_scripts/install-k8s-worker.sh
+++ install_scripts/install-k8s-worker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Enable kernel modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Add some settings to sysctl
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload sysctl
+sudo sysctl --system
+
 echo "Installing docker"
 apt-get update
 apt-get install -y \
@@ -7,14 +21,34 @@ apt-get install -y \
         ca-certificates \
         curl \
         software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-ket add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
         "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
         $(lsb_release -cs) \
         stable"
 
 apt-get update && apt-get install -y \
-        docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
+        docker-ce=$(apt-cache madison docker-ce | grep 20.10 | head -1 | awk '{print $3}')
+
+# Create required directories
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Create daemon json config file
+sudo tee /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Start and enable Services
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
 
 echo "Installing Kubernetes"
 apt-get update && apt-get install -y apt-transport-https
