b6ed8ffce670efd7952092f682632752b6fce6df
sumit <skmaji1@outlook.com>
2022-04-28 09:39:40 +0530

    Updated dashboard configuration

--- install_k8s/install_dashboard.sh
+++ install_k8s/install_dashboard.sh
@@ -4,6 +4,8 @@
 
 source $WORKING_DIR/config
 
+apt-get install net-tools
+
 envsubst < $WORKING_DIR/dashboard/v1.6.3.yaml > $WORKING_DIR/dashboard/dashboard.yaml
 
 kubectl create -f $WORKING_DIR/dashboard/dashboard.yaml
