a79442e8e9258b3f032284269a4ccbd9f6f5d482
sumit <skmaji1@outlook.com>
2022-04-29 21:08:56 +0530

    Creating user for dashboard

--- install_k8s/install_dashboard.sh
+++ install_k8s/install_dashboard.sh
@@ -4,8 +4,33 @@
 
 source $WORKING_DIR/config
 
+mkdir -p $WORKING_DIR/dashboard
+pushd $WORKING_DIR/dashboard
 apt-get install net-tools
 
-envsubst < $WORKING_DIR/dashboard/v2.5.1.yaml > $WORKING_DIR/dashboard/dashboard.yaml
+rm dashboard.key dashboard.crt dashboard.csr
+kubectl delete -f v2.5.1.yaml
+kubectl delete csr dashboard-csr
 
-kubectl create -f $WORKING_DIR/dashboard/dashboard.yaml
+openssl genrsa -out dashboard.key 4096
+openssl req -new -key dashboard.key -out dashboard.csr -subj "/CN=dashboard/O=cloud:masters"
+
+B64=`cat dashboard.csr | base64 | tr -d '\n'`
+cat ../signing-request-template.yaml | \
+    sed "s@__USERNAME__@dashboard@" | \
+    sed "s@__CSRREQUEST__@${B64}@" | \
+    kubectl create -f -
+
+
+kubectl certificate approve dashboard-csr
+
+KEY=`cat dashboard.key | base64 | tr -d '\n'`
+CERT=`kubectl get csr dashboard-csr -o jsonpath='{.status.certificate}'`
+echo $CERT | base64 -d > dashboard.crt
+
+cat v2.5.1.yaml | \
+sed "s@__DASHBOARD_KEY__@${KEY}@" | \
+sed "s@__DASHBOARD_CRT__@${CERT}@" > dashboard.yaml
+cat dashboard.yaml | envsubst | kubectl create -f -
+
+popd
