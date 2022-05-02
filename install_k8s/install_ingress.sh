105420063bd16432f2c0e323ecd25f1390399d49
sumit <skmaji1@outlook.com>
2022-05-02 19:07:46 +0530

    Ingress setup

--- install_k8s/install_ingress.sh
+++ install_k8s/install_ingress.sh
@@ -21,26 +21,31 @@ openssl genrsa -out ${APP_HOST}.key 4096
 openssl req -new -key ${APP_HOST}.key -out ${APP_HOST}.csr -subj "/CN=${APP_HOST}"
 openssl x509 -req -in ${APP_HOST}.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out ${APP_HOST}.crt -days 7200
 
-
-
-cat nginx-ingress-controller-deployment.yaml | \
-sed -i "s/\$LOAD_BALANCER_URL/$LOAD_BALANCER_URL/" | kubectl create -f
-sed -i "s/\$APP_HOST/$APP_HOST/" nginx-ingress.yaml
-sed -i "s/\$APP_HOST/$APP_HOST/" example/app-ingress.yaml
-
 kubectl create namespace ingress
-kubectl create -f default-backend-deployment.yaml -f default-backend-service.yaml -n=ingress
+kubectl create -f default-backend-deployment.yaml -f default-backend-service.yaml
 kubectl create secret tls ingress-certificate --key ingress.key --cert ingress.crt -n ingress
 kubectl create secret tls appingress-certificate --key ${APP_HOST}.key --cert ${APP_HOST}.crt -n default
 kubectl create secret tls appingress-certificate --key ${APP_HOST}.key --cert ${APP_HOST}.crt -n ingress
 kubectl create -f ssl-dh-param.yaml
-kubectl create -f nginx-ingress-controller-config-map.yaml -n=ingress
-kubectl create -f nginx-ingress-controller-roles.yaml -n=ingress
-kubectl create -f nginx-ingress-controller-deployment.yaml -n=ingress
+kubectl create -f nginx-ingress-controller-config-map.yaml
+kubectl create -f nginx-ingress-controller-roles.yaml
+
+cat nginx-ingress-controller-deployment.yaml | \
+sed -r "s/\$LOAD_BALANCER_URL/$(echo "$LOAD_BALANCER_URL" | sed 's/\//\\\//g')/" | \
+kubectl create -f -
+
+cat nginx-ingress.yaml | \
+sed -r "s/\$APP_HOST/$APP_HOST/" | \
+kubectl create -f -
+
 kubectl create -f nginx-ingress.yaml -n=ingress
 kubectl create -f nginx-ingress-controller-service.yaml -n=ingress
 
 #Example app
+cat example/app-ingress.yaml | \
+sed -r "s/\$APP_HOST/$APP_HOST/" | \
+kubectl create -f -
+
 kubectl create -f example/
 
 popd
