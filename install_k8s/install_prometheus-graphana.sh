cc116aa48326d00b2063738c5e49828de53a858b
sumit <skmaji1@outlook.com>
2022-05-04 10:20:26 +0530

    Premetheus and graphana

---
+++ install_k8s/install_prometheus-graphana.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+: ${WORKING_DIR:=$MOUNT_PATH/kubernetes/install_k8s}
+
+source $WORKING_DIR/config
+
+pushd $WORKING_DIR/prometheus-graphana
+kubectl apply -f setup
+kubectl apply -f ../
+kubectl --namespace monitoring patch svc grafana -p '{"spec": {"type": "NodePort"}}'
+kubectl --namespace monitoring patch svc alertmanager-main -p '{"spec": {"type": "NodePort"}}'
+kubectl --namespace monitoring patch svc prometheus-k8s -p '{"spec": {"type": "NodePort"}}'
+
+#https://computingforgeeks.com/setup-prometheus-and-grafana-on-kubernetes/
+##Grapha details
+#Username: admin
+#password: admin
+popd
