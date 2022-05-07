5c54201e62829a50e832ad2d9ecc1711f761370d
sumit <skmaji1@outlook.com>
2022-05-07 11:50:40 +0530

    Graphana configurations

--- install_k8s/install_prometheus-graphana.sh
+++ install_k8s/install_prometheus-graphana.sh
@@ -7,14 +7,23 @@ source $WORKING_DIR/config
 pushd $WORKING_DIR/prometheus-graphana
 kubectl delete -f setup
 kubectl delete -f ../prometheus-graphana
+kubectl delete -f ingress
 
 sleep 15
 
+kubectl create ns monitoring
+
+openssl genrsa -out ${GRAPHANA_HOST}.key 4096
+openssl req -new -key ${GRAPHANA_HOST}.key -out ${GRAPHANA_HOST}.csr -subj "/CN=${GRAPHANA_HOST}" \
+-addext "subjectAltName = DNS:${GRAPHANA_HOST}"
+openssl x509 -req -in ${GRAPHANA_HOST}.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out ${GRAPHANA_HOST}.crt -days 7200
+
+kubectl create secret tls graphanaingress-certificate --key ${GRAPHANA_HOST}.key --cert ${GRAPHANA_HOST}.crt -n monitoring
+
+cat ingress/ingress.yaml | envsubst | kubectl create -f -
 kubectl create -f setup
 kubectl cretae -f ../prometheus-graphana
-kubectl --namespace monitoring patch svc grafana -p '{"spec": {"type": "NodePort"}}'
-kubectl --namespace monitoring patch svc alertmanager-main -p '{"spec": {"type": "NodePort"}}'
-kubectl --namespace monitoring patch svc prometheus-k8s -p '{"spec": {"type": "NodePort"}}'
+kubectl delete networkpolicy graphana -n monitoring
 
 #https://computingforgeeks.com/setup-prometheus-and-grafana-on-kubernetes/
 ##Grapha details
