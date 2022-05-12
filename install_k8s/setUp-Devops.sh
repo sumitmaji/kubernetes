c5ee2569e1f6581e064f32cea8e628358bddd890
sumit <skmaji1@outlook.com>
2022-05-12 22:36:33 +0530

    Devops

--- install_k8s/setUp-Devops.sh
+++ install_k8s/setUp-Devops.sh
@@ -10,10 +10,19 @@ pushd $WORKING_DIR/registry
 ./master.sh
 
 popd
+
 #Setup GitHook
 
-
-
 #Setup DockerHook
+pushd $WORKING_DIR/dockerhook
+
+./run_dockerhook.sh
+
+popd
 
 #Setup RegistryHook
+pushd $WORKING_DIR/reghook
+
+./run_reghook.sh
+
+popd
