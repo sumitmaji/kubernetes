b2608b0da22845532110e12da55e28fc46c1366a
sumit <skmaji1@outlook.com>
2022-05-12 21:07:49 +0530

    Reghook service

--- install_k8s/reghook/run_reghook.sh
+++ install_k8s/reghook/run_reghook.sh
@@ -5,10 +5,10 @@ source scripts/config
 
 : ${PATH_TO_CHART:=chart}
 
-if [[ "$(docker images -q $REPO_NAME 2> /dev/null)" == "" ]]; then
-  ./build.sh
-  ./tag_push.sh
-fi
+#if [[ "$(docker images -q $REPO_NAME 2> /dev/null)" == "" ]]; then
+./build.sh
+./tag_push.sh
+#fi
 
 helm uninstall $RELEASE_NAME
 helm install $RELEASE_NAME $PATH_TO_CHART
