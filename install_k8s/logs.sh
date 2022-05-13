668c39214abb90a221551b7d4c6998ed26a65c34
sumit <skmaji1@outlook.com>
2022-05-13 11:00:43 +0530

    Getting logs util

---
+++ install_k8s/logs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+APP_NAME=$1
+POD_NAME="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.name}")"
+NAMESPACE="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.namespace}")"
+kubectl logs $POD_NAME -n $NAMESPACE
