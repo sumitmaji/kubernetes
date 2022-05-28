f938ae95494597f3e6f0789952894d0106683fc9
sumit <skmaji1@outlook.com>
2022-05-28 07:44:16 +0530

    Clearup of old scripts

--- uninstall_script/uninstall_kube_scheduler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ "TRACE" ]] && set -x
-
-systemctl stop kube-scheduler
-systemctl disable kube-scheduler
-systemctl daemon-reload
-
-rm -rf /etc/systemd/system/kube-scheduler.service
