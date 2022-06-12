6b2461232d65104c5b18ea266e6655c2a4ca3cfe
sumit <skmaji1@outlook.com>
2022-06-12 09:34:57 +0530

    restart the cluster

--- start_cluster.sh
+++ start_cluster.sh
@@ -6,7 +6,7 @@
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 
-/bin/bash /cluster/install_haproxy.sh
+/bin/bash ${MOUNT_PATH}/kubernetes/install_k8s/install_haproxy.sh
 
 systemctl stop kubelet
 systemctl start kubelet
