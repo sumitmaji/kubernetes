02da44c84d3679b0f8b9a069ed5bb44603f17919
sumit <skmaji1@outlook.com>
2022-08-15 16:16:25 +0530

    patching ingress for lets encrypt

---
+++ utils/patch-ingress.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+: ${WORKING_DIR:=$MOUNT_PATH/kubernetes/utils}
+
+source $WORKING_DIR/config
+
+pushd $WORKING_DIR
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -i | --ingress)
+        shift
+        ING=$1
+        ;;
+        -n | --namespace)
+        shift
+        NS=$1
+        ;;
+    esac
+shift
+done
+
+if [ -z "$ING" ]; then
+    echo "Please provide ingress name"
+    exit 1
+elif [ -z "$NS" ]; then
+    echo "Please provide namespace"
+    exit 1
+fi
+
+kubectl patch ingress $ING --patch-file patch-ingress.yaml -n $NS
+kubectl patch ing $ING --type=json -p='[{"op": "replace", "path": "/spec/rules/0/host", "value":"kube.gokcloud.co.in"}]' -n $NS
+
+popd
