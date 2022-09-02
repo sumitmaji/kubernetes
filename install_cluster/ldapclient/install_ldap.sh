e0bdb867ce202ccf384d13c560ad01dbdffb3306
sumit <skmaji1@outlook.com>
2022-09-02 23:11:03 +0530

    Upgrade cluster scripts

---
+++ install_cluster/ldapclient/install_ldap.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+[[ "TRACE" ]] && set -x
+# ./install_ldap.sh -d master.cloud.com -h ldap.master.cloud.com -b dc=master,dc=cloud,dc=com -p sumit -k admin
+: ${ENV:="LOCAL"}
+while [ $# -gt 0 ]; do
+  case "$1" in
+  -d | --domain)
+    shift
+    LDAP_DOMAIN=$1
+    ;;
+  -h | --hostname)
+    shift
+    LDAP_HOSTNAME=$1
+    ;;
+  -b | --basedn)
+    shift
+    BASE_DN=$1
+    ;;
+  -p | --password)
+    shift
+    LDAP_PASSWORD=$1
+    ;;
+  -k | --kpassword)
+    shift
+    KDC_PASSWORD=$1
+    ;;
+  esac
+  shift
+done
+
+DEBIAN_FRONTEND=noninteractive
+LDAP_ORG=CloudInc
+
+echo "ldap domain: $LDAP_DOMAIN"
+echo "ldap hostname: $LDAP_HOSTNAME"
+echo "ldap base_dn: $BASE_DN"
+echo "LDAP_PASSWORD: $LDAP_PASSWORD"
+
+DEBIAN_FRONTEND=noninteractive
+
+# Keep upstart from complaining
+dpkg-divert --local --rename --add /sbin/initctl
+ln -sf /bin/true /sbin/initctl
+DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -yq apt debconf
+apt-get upgrade -yq
+apt-get -y -o Dpkg::Options::="--force-confdef" upgrade
+apt-get -y dist-upgrade
+
+echo "ldap-auth-config ldap-auth-config/rootbindpw password ${LDAP_PASSWORD}" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/bindpw password ${LDAP_PASSWORD}" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/dblogin boolean false" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/override boolean true" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://$LDAP_HOSTNAME" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/pam_password string md5" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/dbrootlogin boolean true" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/binddn string cn=proxyuser,dc=example,dc=net" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/ldapns/ldap_version string 3" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/move-to-debconf boolean true" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/ldapns/base-dn string $BASE_DN" | debconf-set-selections
+echo "ldap-auth-config ldap-auth-config/rootbinddn string cn=admin,$BASE_DN" | debconf-set-selections
+
+apt-get install -yq ldap-auth-client nscd krb5-user libpam-krb5 libpam-ccreds
+
+
+echo "$LDAP_PASSWORD" >/etc/ldap.secret
+chmod 600 /etc/ldap.secret
+
+apt-get install -yq ntp ntpdate nmap libsasl2-modules-gssapi-mit
+
+# Cleanup Apt
+apt-get autoremove
+apt-get autoclean
+apt-get clean
---
+++ install_cluster/ldapclient/utility/bootstrap.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+[[ "TRACE" ]] && set -x
+
+: ${WORKING_DIR:=$MOUNT_PATH/kubernetes/install_cluster/ldapclient}
+
+pushd ${WORKING_DIR}
+
+source config/config
+
+
+: ${ENABLE_KRB:=$ENABLE_KRB}
+: ${REALM:=$(echo $DOMAIN_NAME | tr 'a-z' 'A-Z')}
+: ${DOMAIN_REALM:=$DOMAIN_NAME}
+: ${KERB_MASTER_KEY:=masterkey}
+: ${KERB_ADMIN_USER:=root}
+: ${KERB_ADMIN_PASS:=$KERB_ADMIN_PASS}
+: ${KDC_ADDRESS:=$KDC_ADDRESS}
+: ${LDAP_HOST:=$LDAP_HOST}
+: ${BASE_DN:=$DC}
+: ${LDAP_PASSWORD:=$LDAP_PASSWORD}
+: ${DC_1:=$DC_1}
+: ${DC_2:=$DC_2}
+: ${ENABLE_KUBERNETES:=$ENABLE_KUBERNETES}
+
+fix_nameserver() {
+  cat >/etc/resolv.conf <<EOF
+nameserver $NAMESERVER_IP
+search $SEARCH_DOMAINS
+EOF
+}
+
+fix_hostname() {
+  #sed -i "/^hosts:/ s/ *files dns/ dns files/" /etc/nsswitch.conf
+  if [ "$ENABLE_KUBERNETES" == 'true' ]; then
+    cp /etc/hosts ~/tmp
+    sed -i "s/\([0-9\.]*\)\([\t ]*\)\($(hostname -f)\)/\1 $(hostname -f).$DOMAIN_REALM \3/" ~/tmp
+    cp -f ~/tmp /etc/hosts
+  fi
+}
+
+enable_krb() {
+  mkdir -p /var/log/kerberos
+
+  touch /var/log/kerberos/krb5libs.log
+  touch /var/log/kerberos/krb5kdc.log
+  touch /var/log/kerberos/kadmind.log
+  cat >/etc/krb5.conf <<EOF
+[logging]
+ default = FILE:/var/log/kerberos/krb5libs.log
+ kdc = FILE:/var/log/kerberos/krb5kdc.log
+ admin_server = FILE:/var/log/kerberos/kadmind.log
+
+[libdefaults]
+ default_realm = $REALM
+ dns_lookup_realm = false
+ dns_lookup_kdc = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ proxiable = true
+
+[realms]
+ $REALM = {
+  kdc = $KDC_ADDRESS
+  admin_server = $KDC_ADDRESS
+  database_module = openldap_ldapconf
+ }
+
+[domain_realm]
+ .$DOMAIN_REALM = $REALM
+ $DOMAIN_REALM = $REALM
+
+[dbdefaults]
+        ldap_kerberos_container_dn = cn=krbContainer,$BASE_DN
+
+[dbmodules]
+        openldap_ldapconf = {
+                db_library = kldap
+                ldap_kdc_dn = cn=kdc-srv,ou=krb5,$BASE_DN
+                ldap_kadmind_dn = cn=adm-srv,ou=krb5,$BASE_DN
+                ldap_service_password_file = /etc/krb5kdc/service.keyfile
+                ldap_conns_per_server = 5
+                ldap_servers = $LDAP_HOST
+        }
+EOF
+
+}
+
+enableGss() {
+  sed -i 's/UsePAM no/UsePAM yes/' /etc/ssh/sshd_config
+  echo 'GSSAPIAuthentication yes
+ GSSAPICleanupCredentials yes' >>/etc/ssh/sshd_config
+
+  cat utility/setupUser.sh >> /etc/bash.bashrc
+}
+initializePrincipal() {
+  #Kerberize ssh
+  kadmin -p root/admin -w admin -q "addprinc -randkey host/$(hostname -f)@$REALM"
+  kadmin -p root/admin -w admin -q "xst -k /etc/krb5.keytab host/$(hostname -f)@$REALM"
+}
+
+initialize() {
+  if [ "$ENABLE_KRB" == 'true' ]; then
+    fix_hostname
+    utility/enableKerbPam.sh
+    enable_krb
+    enableGss
+    initializePrincipal
+  else
+    fix_hostname
+    utility/enableLdapPam.sh
+  fi
+
+}
+
+start_ldap() {
+  service nscd start
+  service ssh restart
+}
+
+main() {
+  if [ ! -f /ldap_initialized ]; then
+    initialize
+    start_ldap
+    touch /ldap_initialized
+  else
+    start_ldap
+  fi
+
+  if [[ $1 == "-d" ]]; then
+    while true; do sleep 1000; done
+  else
+    exit 0
+  fi
+}
+
+[[ "$0" == "$BASH_SOURCE" ]] && main "$@"
