d4434444c12ead2e9f6e26d4afe5457728cbe258
sumit <skmaji1@outlook.com>
2022-09-04 09:52:46 +0530

    Upgrade cluster scripts

--- install_cluster/kerberos/install_kerberos.sh
+++ install_cluster/kerberos/install_kerberos.sh
@@ -27,28 +27,6 @@ LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -yq krb5-kdc krb5-admin-
 # kerberos client and pam configuration for kerberos
 apt-get install -yq krb5-user libpam-krb5
 
-echo 'account [success=1 new_authtok_reqd=done default=ignore]        pam_unix.so
-account requisite                       pam_deny.so
-account required                        pam_permit.so
-account required                        pam_krb5.so minimum_uid=1000' >/etc/pam.d/common-account
-
-echo 'auth    [success=2 default=ignore]      pam_krb5.so minimum_uid=1000
-auth    [success=1 default=ignore]      pam_unix.so nullok_secure try_first_pass
-auth    requisite                       pam_deny.so
-auth    required                        pam_permit.so' >/etc/pam.d/common-auth
-
-echo 'password        [success=2 default=ignore]      pam_krb5.so minimum_uid=1000
-password        [success=1 default=ignore]      pam_unix.so obscure use_authtok try_first_pass sha512
-password        requisite                       pam_deny.so
-password        required                        pam_permit.so' >/etc/pam.d/common-password
-
-echo 'session [default=1]                     pam_permit.so
-session requisite                       pam_deny.so
-session required                        pam_permit.so
-session optional                        pam_krb5.so minimum_uid=1000
-session required        pam_unix.so
-session required        pam_mkhomedir.so        skel=/etc/skel umaks=0022' >/etc/pam.d/common-session
-
 # Kerberize sshd
 apt-get install -yq libsasl2-modules-gssapi-mit
 
