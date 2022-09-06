823aaafa35e130b5af7097fbda0f0062fd49baeb
sumit <skmaji1@outlook.com>
2022-09-06 15:49:51 +0530

    Upgrade cluster scripts

--- install_cluster/install_child_node.sh
+++ install_cluster/install_child_node.sh
@@ -2,31 +2,32 @@
 [[ "TRACE" ]] && set -x
 
 while [ $# -gt 0 ]; do
-    case "$1" in
-        -n | --node)
-        shift
-        NODE_NAME=$1
-        ;;
-        -i | --ipaddress)
-        shift
-        IP_ADDRESS=$1
-        ;;
-    esac
-shift
+  case "$1" in
+  -n | --node)
+    shift
+    NODE_NAME=$1
+    ;;
+  -i | --ipaddress)
+    shift
+    IP_ADDRESS=$1
+    ;;
+  esac
+  shift
 done
 
-if [ -z "$NODE_NAME" ]
-then
-	echo "Please provide node name"
-	exit 0
+if [ -z "$NODE_NAME" ]; then
+  echo "Please provide node name"
+  exit 0
 fi
+
+hostnamectl set-hostname "${NODE_NAME}".cloud.com
 
 apt-get update
 apt-get install -y net-tools ifupdown openssh-client openssh-server
 
 rm /etc/netplan/00-installer-config.yaml
 touch /etc/netplan/00-installer-config.yaml
-cat >> /etc/netplan/00-installer-config.yaml << EOF
+cat >>/etc/netplan/00-installer-config.yaml <<EOF
 network:
   renderer: networkd
   ethernets:
@@ -38,19 +39,16 @@ EOF
 netplan generate
 netplan apply
 
-
-if [ ! -f /etc/dhcp/dhclient-enp0s3.conf ]
-then
-touch /etc/dhcp/dhclient-enp0s3.conf
+if [ ! -f /etc/dhcp/dhclient-enp0s3.conf ]; then
+  touch /etc/dhcp/dhclient-enp0s3.conf
 fi
 
-STATUS=`grep -i "send fqdn.fqdn "$NODE_NAME";" /etc/dhcp/dhclient-enp0s3.conf`
-if [ -z "$STATUS" ]
-then
-echo "send fqdn.fqdn \"$NODE_NAME\";" > /etc/dhcp/dhclient-enp0s3.conf
-echo "send fqdn.encoded on;" >> /etc/dhcp/dhclient-enp0s3.conf
-echo "send fqdn.server-update off;" >> /etc/dhcp/dhclient-enp0s3.conf
-echo "also request fqdn.fqdn;" >> /etc/dhcp/dhclient-enp0s3.conf
+STATUS=$(grep -i "send fqdn.fqdn "$NODE_NAME";" /etc/dhcp/dhclient-enp0s3.conf)
+if [ -z "$STATUS" ]; then
+  echo "send fqdn.fqdn \"$NODE_NAME\";" >/etc/dhcp/dhclient-enp0s3.conf
+  echo "send fqdn.encoded on;" >>/etc/dhcp/dhclient-enp0s3.conf
+  echo "send fqdn.server-update off;" >>/etc/dhcp/dhclient-enp0s3.conf
+  echo "also request fqdn.fqdn;" >>/etc/dhcp/dhclient-enp0s3.conf
 fi
 
 chattr -i /etc/resolv.conf
@@ -59,12 +57,10 @@ sed -i '/nameserver 11.0.0.1/ a\nameserver 192.168.0.1' /etc/resolv.conf
 sed -i 's/serach.*/serach cloud.com ./' /etc/resolv.conf
 chattr +i /etc/resolv.conf
 
-
 useradd -m admin
 
-if [ ! -d ~/.ssh ]
-then
-scp -r admin@master:/home/admin/.ssh .
+if [ ! -d ~/.ssh ]; then
+  scp -r admin@master:/home/admin/.ssh .
 fi
 
 ############################################
@@ -76,18 +72,15 @@ fi
 apt-get update
 apt-get install -y sntp libopts25 ntp
 
-STATUS=`grep "server master.cloud.com" /etc/ntp.conf`
-if [ -z "$STATUS" ]
-then
-`sed -i 's/server 0.ubuntu.pool.ntp.org/server master.cloud.com/' /etc/ntp.conf`
-`sed -i 's/server 1.ubuntu.pool.ntp.org//' /etc/ntp.conf`
-`sed -i 's/server 2.ubuntu.pool.ntp.org//' /etc/ntp.conf`
-`sed -i 's/server 3.ubuntu.pool.ntp.org//' /etc/ntp.conf`
+STATUS=$(grep "server master.cloud.com" /etc/ntp.conf)
+if [ -z "$STATUS" ]; then
+  $(sed -i 's/server 0.ubuntu.pool.ntp.org/server master.cloud.com/' /etc/ntp.conf)
+  $(sed -i 's/server 1.ubuntu.pool.ntp.org//' /etc/ntp.conf)
+  $(sed -i 's/server 2.ubuntu.pool.ntp.org//' /etc/ntp.conf)
+  $(sed -i 's/server 3.ubuntu.pool.ntp.org//' /etc/ntp.conf)
 fi
 
 service ntp start
-
-
 
 ############################################
 ############################################
@@ -98,18 +91,16 @@ service ntp start
 apt-get install -y nfs-common
 
 mkdir -p /export
-STATUS=`grep "master:/export    /export   nfs     defaults,_netdev,x-systemd.automount,timeo=14,retry=0        0 0" /etc/fstab`
-if [ -z "$STATUS" ]
-then
-sed -i '$a\master:/export    /export   nfs     defaults,_netdev,x-systemd.automount,timeo=14,retry=0        0 0' /etc/fstab
+STATUS=$(grep "master:/export    /export   nfs     defaults,_netdev,x-systemd.automount,timeo=14,retry=0        0 0" /etc/fstab)
+if [ -z "$STATUS" ]; then
+  sed -i '$a\master:/export    /export   nfs     defaults,_netdev,x-systemd.automount,timeo=14,retry=0        0 0' /etc/fstab
 fi
 mount -a
 
-echo "$NODE_NAME" > /etc/hostname
+echo "$NODE_NAME" >/etc/hostname
 
-echo 'export MOUNT_PATH=/export' >> /etc/bash.bashrc
-echo 'iptables -P FORWARD ACCEPT' >> /root/.bashrc
-
+echo 'export MOUNT_PATH=/export' >>/etc/bash.bashrc
+echo 'iptables -P FORWARD ACCEPT' >>/root/.bashrc
 
 #################################
 #################################
@@ -125,7 +116,7 @@ sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 cp /container/scripts/ssh_config /root/.ssh/
-cat > /root/.ssh/config <<EOF
+cat >/root/.ssh/config <<EOF
 Host *
   UserKnownHostsFile /dev/null
   StrictHostKeyChecking no
@@ -135,10 +126,10 @@ chmod 600 /root/.ssh/config
 chown root:root /root/.ssh/config
 
 # fix the 254 error code
-sed  -i "/^[^#]*UsePAM/ s/.*/#&/"  /etc/ssh/sshd_config
-echo "UsePAM no" >> /etc/ssh/sshd_config
+sed -i "/^[^#]*UsePAM/ s/.*/#&/" /etc/ssh/sshd_config
+echo "UsePAM no" >>/etc/ssh/sshd_config
 
 export NOTVISIBLE="in users profile"
-echo "export VISIBLE=now" >> /etc/profile
+echo "export VISIBLE=now" >>/etc/profile
 
 reboot
