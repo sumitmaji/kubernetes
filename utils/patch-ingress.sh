6c5b9607140c421013a1aa0e5faa6bb2ea6593c8
sumit <skmaji1@outlook.com>
2022-09-14 10:17:17 +0530

    Upgrade cluster scripts

--- utils/patch-ingress.sh
+++ utils/patch-ingress.sh
@@ -7,28 +7,36 @@ source $WORKING_DIR/config
 pushd $WORKING_DIR
 
 while [ $# -gt 0 ]; do
-    case "$1" in
-        -i | --ingress)
-        shift
-        ING=$1
-        ;;
-        -n | --namespace)
-        shift
-        NS=$1
-        ;;
-    esac
-shift
+  case "$1" in
+  -i | --ingress)
+    shift
+    ING=$1
+    ;;
+  -n | --namespace)
+    shift
+    NS=$1
+    ;;
+  -s | --secured)
+    shift
+    SECURED=$1
+    ;;
+  esac
+  shift
 done
 
 if [ -z "$ING" ]; then
-    echo "Please provide ingress name"
-    exit 1
+  echo "Please provide ingress name"
+  exit 1
 elif [ -z "$NS" ]; then
-    echo "Please provide namespace"
-    exit 1
+  echo "Please provide namespace"
+  exit 1
 fi
 
-kubectl patch ingress $ING --patch-file patch-ingress.yaml -n $NS
+kubectl patch ingress $ING --patch-file patch-cert-letsencrypt.yaml -n $NS
 kubectl patch ing $ING --type=json -p='[{"op": "replace", "path": "/spec/rules/0/host", "value":"kube.gokcloud.co.in"}]' -n $NS
+
+if [ $SECURED == "true" ]; then
+  kubectl patch ingress $ING --patch-file patch-ldap-secure.yaml -n $NS
+fi
 
 popd
