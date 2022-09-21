25582cc1acd2725352d91106c9a1c23b5c94041f
sumit <skmaji1@outlook.com>
2022-09-21 12:37:08 +0530

    Upgrade cluster scripts

--- install_k8s/gok.sh
+++ install_k8s/gok.sh
@@ -3,18 +3,23 @@
 release=$2
 CMD=$1
 
-if [ $CMD == "bash" ]; then
-  pod=$(kubectl get po -l app=$release 2>/dev/null | awk "/${release}/" | awk '{print $1}' | head -n 1)
+if [ "$CMD" == "bash" ]; then
+  pod=$(getpod)
   echo "Opening terminal on $pod"
-  kubectl exec -it $pod -- /bin/bash
-elif [ $CMD == "desc" ]; then
-  pod=$(kubectl get po -l app=$release 2>/dev/null | awk  "/${release}/" | awk '{print $1}' | head -n 1)
+  kubectl exec -it "$pod" -- /bin/bash
+elif [ "$CMD" == "desc" ]; then
+  pod=$(getpod)
   echo "Describing pod $pod"
-  kubectl describe po $pod
-elif [ $CMD == "logs" ]; then
-    pod=$(kubectl get po -l app=$release 2>/dev/null | awk  "/${release}/" | awk '{print $1}' | head -n 1)
-    echo "Viewing logs of pod $pod"
-    kubectl logs $pod
-elif [ $CMD == "status" ]; then
-    helm status $release
+  kubectl describe po "$pod"
+elif [ "$CMD" == "logs" ]; then
+  pod=$(getpod)
+  echo "Viewing logs of pod $pod"
+  kubectl logs "$pod"
+elif [ "$CMD" == "status" ]; then
+  helm status "$release"
 fi
+
+getpod() {
+  pod=$(kubectl get po -l app="$release" 2>/dev/null | awk "/${release}/" | awk '{print $1}' | head -n 1)
+  echo "$pod"
+}
