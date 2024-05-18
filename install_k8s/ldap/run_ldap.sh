4490e38054fe392b401d7882677dbd12b778da83
Sumit Kumar Maji <skmaji1@outlook.com>
2024-05-18 15:18:57 -0400

    Restructuring keycloak setup

--- install_k8s/ldap/run_ldap.sh
+++ install_k8s/ldap/run_ldap.sh
@@ -31,7 +31,7 @@ SECRET_NAME=regcred
 kubectl create ns $RELEASE_NAME
 kubectl get secret $SECRET_NAME >/dev/null 2>&1 || kubectl create secret docker-registry \
     $SECRET_NAME --docker-server=$(fullRegistryUrl) --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD -n $RELEASE_NAME
-
+kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'   -n $RELEASE_NAME
 helm uninstall $RELEASE_NAME -n $RELEASE_NAME
 helm install $RELEASE_NAME $PATH_TO_CHART \
   --set image.repository=$(fullRegistryUrl)/ldap \
