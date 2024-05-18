86152bfb3716be22953dc3358296f2ab8abfd9b2
Sumit Kumar Maji <skmaji1@outlook.com>
2024-05-18 14:17:50 -0400

    Restructuring keycloak setup

--- install_k8s/ldap/build.sh
+++ install_k8s/ldap/build.sh
@@ -5,6 +5,7 @@
 source config/config
 source configuration
 docker build --build-arg LDAP_DOMAIN=$DOMAIN_NAME \
+ --build-arg REGISTRY=$(fullRegistryUrl) \
  --build-arg LDAP_HOSTNAME=$LDAP_HOSTNAME --build-arg BASE_DN=$DC  \
  --build-arg LDAP_PASSWORD=sumit -t $IMAGE_NAME .
 
