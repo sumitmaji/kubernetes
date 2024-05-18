d20173b313490db42a6258a6494aaf41a0b18584
Sumit Kumar Maji <skmaji1@outlook.com>
2024-05-18 12:51:43 -0400

    Restructuring keycloak setup

--- install_k8s/logs.sh
+++ install_k8s/logs.sh
@@ -2,4 +2,4 @@
 APP_NAME=$1
 POD_NAME="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.name}")"
 NAMESPACE="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.namespace}")"
-kubectl logs $POD_NAME -n $NAMESPACE
+kubectl logs $POD_NAME -n $NAMESPACE "$@"
