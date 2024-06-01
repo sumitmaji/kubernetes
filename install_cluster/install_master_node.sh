64fcdd0fd1401908cc14f869f3d17cf12a049430
Sumit Kumar Maji <skmaji1@outlook.com>
2024-06-01 08:46:20 -0400

    Setup Child

--- install_cluster/install_master_node.sh
+++ install_cluster/install_master_node.sh
@@ -63,7 +63,8 @@ apt-get -y dist-upgrade
 
 apt-get update
 echo "Installing dhcp server wget nfs-common bind9 ntp gcc make ifupdown net-tools"
-apt-get install -y isc-dhcp-server wget nfs-common bind9 ntp gcc make ifupdown net-tools openssh-server openssh-client
+apt-get install -y isc-dhcp-server wget nfs-common bind9 bind9utils bind9-doc \
+        ntp gcc make ifupdown net-tools openssh-server openssh-client
 
 
 hostnamectl set-hostname master.cloud.com
@@ -77,7 +78,7 @@ network:
   ethernets:
     enp0s3:
       dhcp4: false
-      addresses: [11.0.0.1/24]
+      addresses: [$(getIp).1/24]
       nameservers:
         addresses: [8.8.8.8,8.8.4.4]
     enp0s8:
@@ -94,7 +95,7 @@ elif [ "$ENV" == "CLOUD" ]; then
 [Match]
 Name=eth2
 [Network]
-Address=11.0.0.1
+Address=$(getIp).1
 Mask=255.255.255.0
 EOF
     cat <<EOF >/etc/systemd/network/eth2.netdev
@@ -123,10 +124,12 @@ EOF
   netplan apply
 fi
 
-STATUS="$(grep "##zone append end" /etc/bind/named.conf.default-zones)"
-if [ -z "$STATUS" ]
-then
-cat >>  /etc/bind/named.conf.default-zones << EOF
+
+bindInst(){
+  STATUS="$(grep "##zone append end" /etc/bind/named.conf.default-zones)"
+  if [ -z "$STATUS" ]
+  then
+  cat >>  /etc/bind/named.conf.default-zones << EOF
 ##zone append begin
 zone "cloud.com" {
 	type master;
@@ -141,32 +144,43 @@ zone "$(getRevIp).in-addr.arpa" {
 };
 ##zone append end
 EOF
-fi
+  fi
 
-touch ./rndc-key
-STATUS="$(grep "##rndc-key copy end" ./rndc-key)"
-if [ -z "$STATUS" ]
-then
-echo '##rndc-key copy begin' >  ./rndc-key
-grep -A 3 "key \"rndc-key\"" /etc/bind/rndc.key >> ./rndc-key
-sed -i '$a\##rndc-key copy end' ./rndc-key
-fi
+  touch ./rndc-key
+  STATUS="$(grep "##rndc-key copy end" ./rndc-key)"
+  if [ -z "$STATUS" ]
+  then
+    echo '##rndc-key copy begin' >  ./rndc-key
+    grep -A 3 "key \"rndc-key\"" /etc/bind/rndc.key >> ./rndc-key
+    sed -i '$a\##rndc-key copy end' ./rndc-key
+  fi
 
-STATUS="$(grep "##rndc-key copy end" /etc/bind/named.conf.options)"
-if [ -z "$STATUS" ]
-then
-sed -i '$r ./rndc-key' /etc/bind/named.conf.options
-fi
+  STATUS="$(grep "##rndc-key copy end" /etc/bind/named.conf.options)"
+  if [ -z "$STATUS" ]
+  then
+    sed -i '$r ./rndc-key' /etc/bind/named.conf.options
+  fi
 
-sed -i 's_/etc/bind/\*\* r,_/etc/bind/\*\* rw,_' /etc/apparmor.d/usr.sbin.named
-service apparmor restart
+  if [ "$ENV" == "CLOUD" ]; then
+  cat <<EOF >> /etc/bind/named.conf.options
+acl "trusted" {
+        11.0.0.1;  # ns1 - master
+        11.0.0.2;  # node01
+        11.0.0.3;  # node02
+        11.0.0.4;  # node03
+};
+EOF
+  fi
 
-if [ -f /etc/bind/cloud.com.fwd ]
-then
-echo "The forward exits"
-else
-touch /etc/bind/cloud.com.fwd
-cat >> /etc/bind/cloud.com.fwd << EOF
+  sed -i 's_/etc/bind/\*\* r,_/etc/bind/\*\* rw,_' /etc/apparmor.d/usr.sbin.named
+  service apparmor restart
+
+  if [ -f /etc/bind/cloud.com.fwd ]
+  then
+    echo "The forward exits"
+  else
+  touch /etc/bind/cloud.com.fwd
+  cat >> /etc/bind/cloud.com.fwd << EOF
 \$TTL	86400
 @	IN 		SOA 	master.cloud.com.		root.cloud.com. (
 	1		;Serial
@@ -178,14 +192,14 @@ cat >> /etc/bind/cloud.com.fwd << EOF
 @	IN		NS		master.cloud.com.
 master		IN		A	$(getIp).1
 EOF
-fi
+  fi
 
-if [ -f /etc/bind/cloud.com.rev ]
-then
-echo "The reverse exists"
-else
-touch /etc/bind/cloud.com.rev
-cat >> /etc/bind/cloud.com.rev << EOF
+  if [ -f /etc/bind/cloud.com.rev ]
+  then
+    echo "The reverse exists"
+  else
+  touch /etc/bind/cloud.com.rev
+  cat >> /etc/bind/cloud.com.rev << EOF
 \$TTL    86400
 @       IN              SOA     master.cloud.com.         root.cloud.com. (
         1               ;Serial
@@ -198,21 +212,26 @@ cat >> /etc/bind/cloud.com.rev << EOF
 master.cloud.com  IN      A       $(getIp).1
 1       IN      PTR     master.cloud.com
 EOF
-fi
+  fi
 
-service bind9 restart
+  chmod 775 -R /etc/bind
+  chown -R bind /etc/bind
+  service bind9 restart
 
-if [ -f /etc/dhcp/dhcpd.conf_tmp ]
-then
-echo "Dhcp Temp file exists."
-else
-cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_tmp
-fi
+}
 
-if [[ ! -f /etc/dhcp/temp-local-zones ]]
-then
-cat ./rndc-key > /etc/dhcp/temp-local-zones
-cat >> /etc/dhcp/temp-local-zones << EOF
+dhcpInst(){
+  if [ -f /etc/dhcp/dhcpd.conf_tmp ]
+  then
+    echo "Dhcp Temp file exists."
+  else
+    cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_tmp
+  fi
+
+  if [[ ! -f /etc/dhcp/temp-local-zones ]]
+  then
+    cat ./rndc-key > /etc/dhcp/temp-local-zones
+    cat >> /etc/dhcp/temp-local-zones << EOF
 zone cloud.com. {
         primary 127.0.0.1;
         key rndc-key;
@@ -223,37 +242,35 @@ zone $(getRevIp).in-addr.arpa. {
         key rndc-key;
 }
 EOF
-cat /etc/dhcp/dhcpd.conf_tmp >> /etc/dhcp/temp-local-zones
-sed -i 's/ddns-update-style none/ddns-update-style interim/' /etc/dhcp/temp-local-zones
-sed -i '/ddns-update-style interim/ a\autorotive;' /etc/dhcp/temp-local-zones
-sed -i '/autorotive/ a\ddns-domainname "cloud.com";' /etc/dhcp/temp-local-zones
-sed -i '/ddns-domainname "cloud.com"/ a\ddns-rev-domainname "in-addr.arpa";' /etc/dhcp/temp-local-zones
-sed -i '/ddns-rev-domainname "in-addr.arpa"/ a\ddns-updates on;' /etc/dhcp/temp-local-zones
-sed -i 's/option domain-name "example.org";/option domain-name "cloud.com";/' /etc/dhcp/temp-local-zones
-sed -i "s/option domain-name-servers ns1.example.org, ns2.example.org;/option domain-name-servers $(getIp).1, 192.168.0.1;/" /etc/dhcp/temp-local-zones
-sed -i "/^max-lease-time 7200;/ a\subnet $(getIp).0 netmask 255.255.255.0 {" /etc/dhcp/temp-local-zones
-sed -i "/subnet $(getIp).0 netmask 255.255.255.0 {/ a\option routers $(getIp).1;"  /etc/dhcp/temp-local-zones
-sed -i "/option routers $(getIp).1;/ a\option subnet-mask 255.255.255.0;" /etc/dhcp/temp-local-zones
-sed -i "/option subnet-mask 255.255.255.0;/ a\option time-offset -18000;" /etc/dhcp/temp-local-zones
-sed -i "/option time-offset -18000;/ a\range $(getIp).1 $(getIp).254;" /etc/dhcp/temp-local-zones
-sed -i "/range $(getIp).1 $(getIp).254;/ a\}" /etc/dhcp/temp-local-zones
+    cat /etc/dhcp/dhcpd.conf_tmp >> /etc/dhcp/temp-local-zones
+    sed -i 's/ddns-update-style none/ddns-update-style interim/' /etc/dhcp/temp-local-zones
+    sed -i '/ddns-update-style interim/ a\autorotive;' /etc/dhcp/temp-local-zones
+    sed -i '/autorotive/ a\ddns-domainname "cloud.com";' /etc/dhcp/temp-local-zones
+    sed -i '/ddns-domainname "cloud.com"/ a\ddns-rev-domainname "in-addr.arpa";' /etc/dhcp/temp-local-zones
+    sed -i '/ddns-rev-domainname "in-addr.arpa"/ a\ddns-updates on;' /etc/dhcp/temp-local-zones
+    sed -i 's/option domain-name "example.org";/option domain-name "cloud.com";/' /etc/dhcp/temp-local-zones
+    sed -i "s/option domain-name-servers ns1.example.org, ns2.example.org;/option domain-name-servers $(getIp).1, 192.168.0.1;/" /etc/dhcp/temp-local-zones
+    sed -i "/^max-lease-time 7200;/ a\subnet $(getIp).0 netmask 255.255.255.0 {" /etc/dhcp/temp-local-zones
+    sed -i "/subnet $(getIp).0 netmask 255.255.255.0 {/ a\option routers $(getIp).1;"  /etc/dhcp/temp-local-zones
+    sed -i "/option routers $(getIp).1;/ a\option subnet-mask 255.255.255.0;" /etc/dhcp/temp-local-zones
+    sed -i "/option subnet-mask 255.255.255.0;/ a\option time-offset -18000;" /etc/dhcp/temp-local-zones
+    sed -i "/option time-offset -18000;/ a\range $(getIp).1 $(getIp).254;" /etc/dhcp/temp-local-zones
+    sed -i "/range $(getIp).1 $(getIp).254;/ a\}" /etc/dhcp/temp-local-zones
 
-cat /etc/dhcp/temp-local-zones > /etc/dhcp/dhcpd.conf
-fi
+    cat /etc/dhcp/temp-local-zones > /etc/dhcp/dhcpd.conf
+  fi
 
-service isc-dhcp-server restart
+  service isc-dhcp-server restart
+}
 
-chattr -i /etc/resolv.conf
-sed -i "/nameserver/ i nameserver $(getIp).1" /etc/resolv.conf
-sed -i "/nameserver $(getIp).1/ a\nameserver 192.168.0.1" /etc/resolv.conf
-sed -i 's/search.*/search cloud.com ./' /etc/resolv.conf
-chattr +i /etc/resolv.conf
 
-chmod 775 -R /etc/bind
-chown -R bind /etc/bind
-service bind9 restart
-service isc-dhcp-server restart
-
+nameserver(){
+  chattr -i /etc/resolv.conf
+  sed -i "/nameserver/ i nameserver $(getIp).1" /etc/resolv.conf
+  sed -i "/nameserver $(getIp).1/ a\nameserver 192.168.0.1" /etc/resolv.conf
+  sed -i 's/search.*/search cloud.com ./' /etc/resolv.conf
+  chattr +i /etc/resolv.conf
+}
 
 ###########################
 ###########################
@@ -261,29 +278,29 @@ service isc-dhcp-server restart
 ###########################
 ###########################
 ###########################
-
-apt-get install -y iptables-persistent <<EOF
+natInst(){
+  apt-get install -y iptables-persistent <<EOF
 YES
 YES
 EOF
-echo "1" > /proc/sys/net/ipv4/ip_forward
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-if [ "$ENV" == "LOCAL" ]; then
-  iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
-elif [ "$ENV" == "CLOUD" ]; then
-   iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-fi
-iptables-save > /etc/iptables/rules.v4
+  echo "1" > /proc/sys/net/ipv4/ip_forward
+  sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+  if [ "$ENV" == "LOCAL" ]; then
+    iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
+  elif [ "$ENV" == "CLOUD" ]; then
+     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  fi
+  iptables-save > /etc/iptables/rules.v4
 
-if [ "$ENV" == "LOCAL" ]; then
-      ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
-      cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+  if [ "$ENV" == "LOCAL" ]; then
+    ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
+    cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
 
-      useradd -m admin
-      cp ~/.ssh/ /home/admin/ -r
-      chown -R admin /home/admin/
-fi
-
+    useradd -m admin
+    cp ~/.ssh/ /home/admin/ -r
+    chown -R admin /home/admin/
+  fi
+}
 
 #################################
 #################################
@@ -291,9 +308,10 @@ fi
 #################################
 #################################
 
-sed -i "/^restrict ::1$/a\ restrict $(getIp).0 mask 255.255.255.0 nomodify notrap" /etc/ntp.conf
-service ntp start
-
+ntpInst(){
+  sed -i "/^restrict ::1$/a\ restrict $(getIp).0 mask 255.255.255.0 nomodify notrap" /etc/ntp.conf
+  service ntp start
+}
 
 #################################
 #################################
@@ -301,22 +319,23 @@ service ntp start
 #################################
 #################################
 
+nfsInst(){
+  apt-get install -y nfs-kernel-server
 
-apt-get install -y nfs-kernel-server
-
-echo "/export $(getIp).0/24(rw,async,no_root_squash)" >> /etc/exports
-mkdir -p /export
-chmod 777 /export
-exportfs -va
-systemctl start nfs-kernel-server
-systemctl enable nfs-kernel-server
-mount $(getIp).1:/export /export
+  echo "/export $(getIp).0/24(rw,async,no_root_squash)" >> /etc/exports
+  mkdir -p /export
+  chmod 777 /export
+  exportfs -va
+  systemctl start nfs-kernel-server
+  systemctl enable nfs-kernel-server
+  mount $(getIp).1:/export /export
 
 
-echo 'export MOUNT_PATH=/export' >> /etc/bash.bashrc
-echo 'iptables -P FORWARD ACCEPT' >> /root/.bashrc
+  echo 'export MOUNT_PATH=/export' >> /etc/bash.bashrc
+  echo 'iptables -P FORWARD ACCEPT' >> /root/.bashrc
 
-rm -rf rndc-key
+  rm -rf rndc-key
+}
 
 #################################
 #################################
@@ -324,28 +343,33 @@ rm -rf rndc-key
 #################################
 #################################
 
-mkdir -p /var/run/sshd
-echo 'root:root' | chpasswd
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sshdInst(){
+  mkdir -p /var/run/sshd
+  echo 'root:root' | chpasswd
+  sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-# SSH login fix. Otherwise user is kicked off after login
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+  # SSH login fix. Otherwise user is kicked off after login
+  sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
-cp /container/scripts/ssh_config /root/.ssh/
-cat > /root/.ssh/config <<EOF
+  cp /container/scripts/ssh_config /root/.ssh/
+  cat > /root/.ssh/config <<EOF
 Host *
   UserKnownHostsFile /dev/null
   StrictHostKeyChecking no
   LogLevel quiet
 EOF
-chmod 600 /root/.ssh/config
-chown root:root /root/.ssh/config
+  chmod 600 /root/.ssh/config
+  chown root:root /root/.ssh/config
 
-# fix the 254 error code
-sed  -i "/^[^#]*UsePAM/ s/.*/#&/"  /etc/ssh/sshd_config
-echo "UsePAM no" >> /etc/ssh/sshd_config
+  # fix the 254 error code
+  sed  -i "/^[^#]*UsePAM/ s/.*/#&/"  /etc/ssh/sshd_config
+  echo "UsePAM no" >> /etc/ssh/sshd_config
 
-export NOTVISIBLE="in users profile"
-echo "export VISIBLE=now" >> /etc/profile
+  export NOTVISIBLE="in users profile"
+  echo "export VISIBLE=now" >> /etc/profile
+}
 
-reboot
+if [ "$ENV" == "CLOUD" ]; then
+  bindInst
+fi
+
