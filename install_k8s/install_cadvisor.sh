8a82b4d90cda8c1755e02c34e0684dec7525cdb3
Sumit Kumar Maji <skmaji1@outlook.com>
2024-06-17 23:57:33 -0400

    Removing redundant scripts

--- install_k8s/install_cadvisor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: ${WORKING_DIR:=$MOUNT_PATH/kubernetes/install_k8s}
-
-source $WORKING_DIR/config
-
-pushd $WORKING_DIR/cadvisor
-kubectl apply -f daemon.yaml
-popd
--- install_k8s/logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-APP_NAME=$1
-POD_NAME="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.name}")"
-NAMESPACE="$(kubectl get po -l=k8s-app=$APP_NAME --all-namespaces -o jsonpath="{.items[0].metadata.namespace}")"
-kubectl logs $POD_NAME -n $NAMESPACE "$@"
