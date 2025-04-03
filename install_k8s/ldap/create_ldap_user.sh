f2a18ec16d41d06397a28d89d2e48d45671d0531
Sumit Maji <skmaji1@outlook.com>
2025-04-02 23:05:29 -0400

    feat: add functions to update LDAP configuration and user data, and create a Kubernetes job for user creation

---
+++ install_k8s/ldap/create_ldap_user.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+  
+  username="$USERNAME"
+  user_password="$USER_PASSWORD"
+  email="$EMAIL"
+  first_name="$FIRST_NAME"
+  last_name="$LAST_NAME"
+  group_name="$GROUP_NAME"
+
+  echo "Creating LDAP user: $username"
+  
+  gid=$(ldapsearch -x -b "ou=groups,$BASE_DN" "cn=$group_name" -D "cn=admin,$BASE_DN" -w ${password} -H ${LDAP_HOSTNAME} -LLL gidNumber | grep 'gidNumber' | grep -Eo '[0-9]+')
+  
+  # Create the user using ldapadd
+  ldapadd -x -D "cn=admin,${BASE_DN}" -w "${password}" <<EOF
+dn: cn=${username},ou=users,${BASE_DN}
+gidnumber: $gid
+givenname: $first_name
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: top
+homedirectory: /home/users/$username
+loginshell: /bin/bash
+uid: ${username}
+cn: ${username}
+sn: ${last_name}
+userpassword: ${user_password}
+mail: ${email}
+uidnumber: $((RANDOM % 10000 + 1000))
+EOF
+
+  if [[ $? -ne 0 ]]; then
+    echoFailed "Failed to create LDAP user: $username"
+    return 1
+  fi
+
+  echoSuccess "LDAP user $username created successfully!"
+  return 0
