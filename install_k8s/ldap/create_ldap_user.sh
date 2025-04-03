3b28f08e6619074c11d9f51c8746d0a2849651dd
Sumit Maji <skmaji1@outlook.com>
2025-04-02 23:49:54 -0400

    fix: delete ldapsecret during user creation and comment out debug environment variable prints

--- install_k8s/ldap/create_ldap_user.sh
+++ install_k8s/ldap/create_ldap_user.sh
@@ -9,16 +9,17 @@
 
   echo "Creating LDAP user: $username"
 
-  echo "Environment variables:"
-  echo "USERNAME: $username"
-  echo "USER_PASSWORD: $user_password"
-  echo "EMAIL: $email"
-  echo "FIRST_NAME: $first_name"
-  echo "LAST_NAME: $last_name"
-  echo "GROUP_NAME: $group_name"
-  echo "BASE_DN: $BASE_DN"
-  echo "LDAP_HOSTNAME: $LDAP_HOSTNAME"
-  echo "Admin Password: $password"
+  # Print environment variables for debugging purposes
+  # echo "Environment variables:"
+  # echo "USERNAME: $username"
+  # echo "USER_PASSWORD: $user_password"
+  # echo "EMAIL: $email"
+  # echo "FIRST_NAME: $first_name"
+  # echo "LAST_NAME: $last_name"
+  # echo "GROUP_NAME: $group_name"
+  # echo "BASE_DN: $BASE_DN"
+  # echo "LDAP_HOSTNAME: $LDAP_HOSTNAME"
+  # echo "Admin Password: $password"
   gid=$(ldapsearch -x -b "ou=groups,$BASE_DN" "cn=$group_name" -D "cn=admin,$BASE_DN" -w ${password} -H ${LDAP_HOSTNAME} -LLL gidNumber | grep 'gidNumber' | grep -Eo '[0-9]+')
   
   echo "Found gidNumber for group '$group_name': $gid"
