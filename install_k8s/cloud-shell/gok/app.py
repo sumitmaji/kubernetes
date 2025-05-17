bc470c1b04a30b639c39367347273a09e0762855
Sumit Maji <skmaji1@outlook.com>
2025-05-17 17:10:46 -0400

    fix: update auth-url in ingress configuration to point to the new OAuth2 endpoint

--- install_k8s/cloud-shell/gok/app.py
+++ install_k8s/cloud-shell/gok/app.py
@@ -230,7 +230,7 @@ def ensure_ttyd_ingress(username):
                 "metadata": {
                     "name": ingress_name,
                     "annotations": {
-                        "nginx.ingress.kubernetes.io/auth-url": "https://cloudshell.gokcloud.com/shell/validate",
+                        "nginx.ingress.kubernetes.io/auth-url": "https://kube.gokcloud.com/oauth2/auth",
                         "nginx.ingress.kubernetes.io/auth-signin": f"https://kube.gokcloud.com/oauth2/start?rd=https://cloudshell.gokcloud.com/user/{username}",
                         "nginx.ingress.kubernetes.io/auth-response-headers": "Authorization",
                         "kubernetes.io/ingress.class": "nginx",
