a1d44845b0dd2eb0b3ea339db8c7eaf4b7347d1e
Sumit Maji <skmaji1@outlook.com>
2025-05-26 15:16:47 -0400

    feat: Enhance LDAP installation script to accept dynamic passwords and update secret templates

--- install_k8s/ldap/run_ldap.sh
+++ install_k8s/ldap/run_ldap.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 source configuration
-
 source $MOUNT_PATH/kubernetes/install_k8s/util
-
 : ${PATH_TO_CHART:=chart}
 
-while [ $# -gt 0 ]; do
-    case "$1" in
-        -u | --user)
-        shift
-        DOCKER_USER=$1
-        ;;
-        -p | --password)
-        shift
-        DOCKER_PASSWORD=$1
-        ;;
-    esac
-shift
-done
+LDAP_PASSWORD="$1"
+KERBEROS_PASSWORD="$2"
+KERBEROS_KDC_PASSWORD="$3"
+KERBEROS_ADM_PASSWORD="$4"
 
-: ${PATH_TO_CHART:=chart}
+if [[ -z "$LDAP_PASSWORD" || -z "$KERBEROS_PASSWORD" || -z "$KERBEROS_KDC_PASSWORD" || -z "$KERBEROS_ADM_PASSWORD" ]]; then
+    echo "Usage: $0 <ldap_password> <kerberos_password> <kerberos_kdc_password> <kerberos_adm_password>"
+    return 1
+fi
 
-#if [[ "$(docker images -q $REPO_NAME 2> /dev/null)" == "" ]]; then
+
 ./build.sh
 ./tag_push.sh
-#fi
 
+NS=ldap
+helm uninstall $RELEASE_NAME -n $NS || true
 
-helmInst $RELEASE_NAME $REPO_NAME ldap
+SECRET_NAME=regcred
+kubectl create namespace $NS >/dev/null 2>&1 || true
+kubectl get secret $SECRET_NAME >/dev/null 2>&1 || kubectl create secret docker-registry \
+    $SECRET_NAME --docker-server=$(fullRegistryUrl) --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD -n $NS
+kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}' -n $NS
+helm install $RELEASE_NAME $PATH_TO_CHART \
+--set image.repository=$(fullRegistryUrl)/$REPO_NAME \
+--namespace $NS \
+--set ldap.password="$LDAP_PASSWORD" \
+--set kerberos.passwrd="$KERBEROS_USER" \
+--set kerberos.kdcpassword="$KERBEROS_KDC_PASSWORD" \
+--set kerberos.admpassword="$KERBEROS_ADM_PASSWORD"
+
+echo "Waiting for services to be up!!!!"
+kubectl --timeout=180s wait --for=condition=Ready pods --all --namespace "$NS"
+echoSuccess "$REPO_NAME service is up!!"
