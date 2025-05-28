41f7554da563474224f2827a74dff97ed16bb549
Sumit Maji <skmaji1@outlook.com>
2025-05-27 22:17:44 -0400

    fix: Update SocketIO initialization to include custom path for socket connections

--- install_k8s/gok-agent/controller/backend/app.py
+++ install_k8s/gok-agent/controller/backend/app.py
@@ -113,8 +113,8 @@ app = Flask(
     static_folder="static",  # This is where your React build is copied
     static_url_path=""       # Serve static files at root
 )
-socketio = SocketIO(app, cors_allowed_origins="*")
-
+#socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", path="/controller/socket.io")
 # Initial load
 vault_secrets = get_vault_secrets()
 app.config["API_TOKEN"] = vault_secrets.get("api-token", "changeme")
