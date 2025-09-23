dede6f88fad7ef058b1c8e739e6b376b7d8d399f
Sumit Maji <skmaji1@outlook.com>
2025-09-22 21:00:05 -0400

    fix: Enhance login script to extract access_token and create kubeconfig with token

--- install_k8s/gok-login/login.sh
+++ install_k8s/gok-login/login.sh
@@ -27,4 +27,38 @@ else
     -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}")
 fi
 
-echo "Response: $RESPONSE"
+
+# Extract access_token from response
+ACCESS_TOKEN=$(echo "$RESPONSE" | grep -o '"access_token":"[^"]*"' | cut -d':' -f2 | tr -d '"')
+
+if [ -z "$ACCESS_TOKEN" ]; then
+  echo "Error: access_token not found in response."
+  exit 2
+fi
+
+echo "access_token extracted."
+
+mkdir -p /tools
+export KUBECONFIG=/tools/kubeconfig
+# Create kubeconfig file
+cat <<EOF > /tools/kubeconfig
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    server: https://10.0.0.244:6443
+    insecure-skip-tls-verify: true
+  name: k8s
+users:
+- name: user
+  user:
+    token: $ACCESS_TOKEN
+contexts:
+- context:
+    cluster: k8s
+    user: user
+  name: k8s
+current-context: k8s
+EOF
+
+echo "Kubeconfig created at /tools/kubeconfig"
